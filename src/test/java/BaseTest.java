5f2de7323f4f7dc99d3d9895134031f3944ee2a0
darkpool77 <darkpool77@users.noreply.github.com>
2023-03-23 22:34:54 -0700

    s

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,6 +1,5 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.safari.SafariDriver;
@@ -10,9 +9,39 @@ import org.testng.annotations.BeforeSuite;
 
 public class BaseTest {
 
+    public static WebDriver driver;
+
     @BeforeSuite
     static void setupClass() {
         WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeMethod
+    public static void launchBrowser(String browser) {
+        driver = pickBrowser(System.getProperty("browser"));
+
+
+    }
+
+    private static WebDriver pickBrowser(String browser) {
+        switch (browser) {
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                return driver = new FirefoxDriver();
+
+            case "safari":
+                WebDriverManager.safaridriver().setup();
+                return driver = new SafariDriver();
+
+            default:
+                return driver = new ChromeDriver();
+
+
+
+
+
+        }
+
     }
 
 }
