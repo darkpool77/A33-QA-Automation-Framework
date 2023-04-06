d178f5527a900cce37a098156ee43dbe440cbb84
darkpool77 <darkpool77@users.noreply.github.com>
2023-04-06 14:21:14 -0700

    2

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -2,9 +2,14 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.safari.SafariDriver;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
+
+import java.net.MalformedURLException;
+import java.net.URI;
 
 
 public class BaseTest {
@@ -17,13 +22,15 @@ public class BaseTest {
     }
 
     @BeforeMethod
-    public static void launchBrowser() {
+    public static void launchBrowser() throws MalformedURLException {
         driver = pickBrowser( System.getProperty("browser"));
 
 
     }
 
-    private static WebDriver pickBrowser(String browser) {
+    private static WebDriver pickBrowser(String browser) throws MalformedURLException {
+        DesiredCapabilities caps = new DesiredCapabilities();
+        String gridURL = "http://192.168.0.49:4444";
         switch (browser) {
             case "firefox":
                 WebDriverManager.firefoxdriver().setup();
@@ -33,8 +40,23 @@ public class BaseTest {
                 WebDriverManager.safaridriver().setup();
                 return driver = new SafariDriver();
 
+            case "grid-safari":
+                caps.setCapability("browserName", "Safari");
+                return driver = new RemoteWebDriver(URI.create(gridURL).toURL(), caps);
+
+            case "grid-firefox":
+                caps.setCapability("browserName", "firefox");
+                return driver = new RemoteWebDriver(URI.create(gridURL).toURL(), caps);
+
+            case "grid-chrome":
+                caps.setCapability("browserName", "chrome");
+                return driver = new RemoteWebDriver(URI.create(gridURL).toURL(), caps);
+
+
             default:
                 return driver = new ChromeDriver();
+
+
 
         }
 
